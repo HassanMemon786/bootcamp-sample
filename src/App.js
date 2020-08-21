6d2781ff7d6625d4e813f3a8c531b4c1ab139365
Hassan Memon <51920441+HassanMemon786@users.noreply.github.com>
2020-08-21 16:17:34 -0700

    Update session 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Message } from './Message';
 import './App.css';
 
 function App() {
+  let [count, setCount] = useState(1);
+  let [isMor, setMor] = useState(false);
+
   return (
-    <div className='App'>
-      <h1>Hello World from all of us</h1>
+    <div className={`box ${isMor ? 'dayLight' : ''}`}>
+      <h1>Good {isMor ? 'Morning' : 'Night'}</h1>
+      <Message counter={count} />
+      <br />
+      <button onClick={() => setCount(++count)}>Update Counter</button>
+      <br />
+      <button onClick={() => setMor(!isMor)}>
+        {isMor ? 'Night Mode' : 'Day Mode'}
+      </button>
     </div>
   );
 }
---
+++ src/dinner.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function dinner(props) {
+  return <h1>Tody we serving {props.dishName} </h1>;
+}
+
+export default dinner;
